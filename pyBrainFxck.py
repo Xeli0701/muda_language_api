8901fe11e48d06649cb78f8197a87bb222a7c886
Xeli0701 <yamato232@live.jp>
2021-06-19 13:12:41 +0900

    :bookmark: Version1 Mudalang to Brainfxck

--- pyBrainFxck.py
+++ pyBrainFxck.py
@@ -1,99 +1,104 @@
-def pyBrainFxck(code):
-    #メモリポインタ
-    pointer = 0
+def Muda_language_execute(muda_code):
+    code = Muda_language2BrainFxck_code(muda_code)
+    return pyBrainFxck(code)
 
-    #プログラムカウンタ
-    program_counter = 0
-
-    #メモリ
-    memory = [0 for i in range(10000)]
-
-
-    #変換処理
+def Muda_language2BrainFxck_code(muda_code):
     """
-    > muda
-    < MUDA
-    . ムダﾞ
-    + むだ
-    - 無駄
-    , ﾑﾀﾞ
-    [ 、
-    ] 。
+    無駄言語の変換処理
+    #Muda_language to BrainFxck_code
+
+    DocTest
+    >>> Muda_language2BrainFxck_code("むだむだむだむだむだむだむだむだ、mudaむだむだむだむだ、mudaむだむだmudaむだむだむだmudaむだむだむだmudaむだMUDAMUDAMUDAMUDA無駄。mudaむだmudaむだmuda無駄mudamudaむだ、MUDA。MUDA無駄。mudamudaムダmuda無駄無駄無駄ムダむだむだむだむだむだむだむだムダムダむだむだむだムダmudamudaムダMUDA無駄ムダMUDAムダむだむだむだムダ無駄無駄無駄無駄無駄無駄ムダ無駄無駄無駄無駄無駄無駄無駄無駄ムダmudamudaむだムダmudaむだむだムダ")
+    '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'
     """
-    code_temp = code.replace("muda",">")
+    code_temp = muda_code.replace("muda",">")
     code_temp = code_temp.replace("MUDA","<")
     code_temp = code_temp.replace("ムダ",".")
     code_temp = code_temp.replace("むだ","+")
     code_temp = code_temp.replace("無駄","-")
     code_temp = code_temp.replace("ﾑﾀﾞ",",")
     code_temp = code_temp.replace("、","[")
-    tape      = code_temp.replace("。","]")
-    print(tape)
+    code      = code_temp.replace("。","]")
 
+    return code
+
+def pyBrainFxck(code):
+    """
+    Python上でBrainFxckを実行するコンパイラ
+    入れられたcodeを処理し、その結果出力される文章を返却する。
+
+    BrainFxck仕様
+    ">" メモリポインタをインクリメント
+    "<" メモリポインタをデクリメント
+    "." メモリポインタが指し示す先に格納された変数を処理系に表示
+    "+" メモリポインタが指し示す先に格納された変数をインクリメント
+    "-" メモリポインタが指し示す先に格納された変数をデクリメント
+    "," 標準入力された値をメモリポインタが指し示す先に代入
+    "[" メモリポインタが指す値が0なら、対応する"]"までskip
+    "]" メモリポインタが指す値が非0なら、対応する"["までジャンプ
+    →これらの分岐が終わったらプログラムカウンタをインクリメント
+
+    DocTest
+    >>> pyBrainFxck("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+    'Hello World!\\n'
+    >>> pyBrainFxck("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.>++++++++++.")
+    'ABC\\n'
+    """
+    #メモリポインタ・プログラムカウンタ・メモリ定義
+    pointer = 0
+    program_counter = 0
+    memory = [0 for i in range(10000)]
+
+    #結果
     result = ""
 
-    #コードの末尾までプログラムを処理する
-    while program_counter < len(tape):
-
-        #プログラム・カウンタが指し示す文字を読み ">"ならメモリポインタをインクリメントする
-        if tape[program_counter] == '>':
+    #実行部分
+    while program_counter < len(code):
+        if code[program_counter] == '>':
             pointer += 1
-
-        #"<" メモリポインタをデクリメントする
-        elif tape[program_counter] == '<':
+        elif code[program_counter] == '<':
             pointer -= 1
-
-        #"." メモリポインタが指し示す先に格納された変数を処理系に表示
-        elif tape[program_counter] == '.':
+        elif code[program_counter] == '.':
             result += chr(memory[pointer])
-
-        #"+" メモリポインタが指し示す先に格納された変数をインクリメント
-        elif tape[program_counter] == '+':
+        elif code[program_counter] == '+':
             memory[pointer] += 1
-
-        #"-" メモリポインタが指し示す先に格納された変数をデクリメント
-        elif tape[program_counter] == '-':
+        elif code[program_counter] == '-':
             memory[pointer] -= 1
-
-        #"," 標準入力された値をメモリポインタが指し示す先に代入
-        elif tape[program_counter] == ',':
+        elif code[program_counter] == ',':
             memory[pointer] = int(input())
-
-        #"[" メモリポインタが指す値が0なら、対応する"]"までskip
-        elif tape[program_counter] == '[':
+        elif code[program_counter] == '[':
             if memory[pointer] == 0:
-                #nest構造をカウントする変数
+                #nestカウント
                 nest = 0
                 while True:
                     program_counter += 1
-                    if tape[program_counter] == '[':
+                    if code[program_counter] == '[':
                         nest += 1
-                    if tape[program_counter] == ']' and nest == 0:
+                    if code[program_counter] == ']' and nest == 0:
                         break
-                    if tape[program_counter] == ']':
+                    if code[program_counter] == ']':
                         nest -= 1
-        #"]" メモリポインタが指す値が非0なら、対応する"["までジャンプ
-        elif tape[program_counter] == ']':
+        elif code[program_counter] == ']':
             if memory[pointer] != 0:
-                #nest構造をカウントする変数
+                #nestカウント
                 nest = 0
                 while True:
                     program_counter -= 1
-                    if tape[program_counter] == ']':
+                    if code[program_counter] == ']':
                         nest += 1
-                    if tape[program_counter] == '[' and nest == 0:
+                    if code[program_counter] == '[' and nest == 0:
                         break
-                    if tape[program_counter] == '[':
+                    if code[program_counter] == '[':
                         nest -= 1
-                        
-        #プログラム・カウンタをインクリメントする
         program_counter += 1
 
     return result
 
 if __name__ == '__main__':
-    code = """
+    muda_code = """
     むだむだむだむだむだむだむだむだ、mudaむだむだむだむだ、mudaむだむだmudaむだむだむだmudaむだむだむだmudaむだMUDAMUDAMUDAMUDA無駄。mudaむだmudaむだmuda無駄mudamudaむだ、MUDA。MUDA無駄。mudamuda
     ムダmuda無駄無駄無駄ムダむだむだむだむだむだむだむだムダムダむだむだむだムダmudamudaムダMUDA無駄ムダMUDAムダむだむだむだムダ無駄無駄無駄無駄無駄無駄ムダ無駄無駄無駄無駄無駄無駄無駄無駄ムダmudamudaむだムダmudaむだむだムダ
     """
-    print(pyBrainFxck(code))
+    print(Muda_language_execute(muda_code))
+    import doctest
+    doctest.testmod()
